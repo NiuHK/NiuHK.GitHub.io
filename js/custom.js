179e66fdc336dbaac98446b9614524c374b84158
苍术 <129147116+NiuHK@users.noreply.github.com>
2024-03-13 06:56:36 +0000

    Site updated: 2024-03-13 06:56:36

---
+++ js/custom.js
@@ -0,0 +1,20 @@
+var xpath = "/html/body/section/div/div/div[1]/div[1]/article/div[2]/h2";
+var h2Elements = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+for (let i = 0; i < h2Elements.snapshotLength; i++) {
+    let h2Element = h2Elements.snapshotItem(i);
+    var spanElement = document.createElement('span');
+    spanElement.textContent = h2Element.textContent;
+    spanElement.className = 'custom-h2';
+    h2Element.innerHTML = '';
+    h2Element.appendChild(spanElement);
+}
+var xpath = "/html/body/section/div/div/div[1]/div[1]/article/div[2]/h1";
+var h2Elements = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+for (let i = 0; i < h2Elements.snapshotLength; i++) {
+    let h2Element = h2Elements.snapshotItem(i);
+    var spanElement = document.createElement('span');
+    spanElement.textContent = h2Element.textContent;
+    spanElement.className = 'custom-h1';
+    h2Element.innerHTML = '';
+    h2Element.appendChild(spanElement);
+}
